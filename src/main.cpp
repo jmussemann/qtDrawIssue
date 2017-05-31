30756b3d0eb17f5775e399029b6eb64a42d5fb89
jmussemann <jmussemann@gmail.com>
2017-05-31 11:24:22 -0700

    fixed stupid git collisions

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <QApplication>
 #include "mainwindow.h"
 
@@ -19,25 +18,3 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR /*lpCmdLi
    return main(__argc,__argv);
 }
 #endif
-=======
-#include <QApplication>
-#include "mainwindow.h"
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-
-    return a.exec();
-}
-
-#if defined(_MSC_VER)
-#include <windows.h>
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR /*lpCmdLine*/, int /*nCmdShow*/)
-{
-   return main(__argc,__argv);
-}
-#endif
->>>>>>> 9d18f777fde0dd673667c4b8b8ed4bff591f7162
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -35,75 +34,4 @@ protected slots:
    void update();
    void rateChanged(bool);
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #endif
-=======
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
-#include <QMainWindow>
-#include <QtCore>
-#include <QtGui>
-#include <QTimer>
-#include <QShortcut>
-#include <QTextBrowser>
-
-#include "boost/date_time/posix_time/posix_time.hpp"
-
-namespace Ui {
-    class MainWindow;
-}
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT;
-
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-
-protected:
-
-
-private:
-    Ui::MainWindow *ui;
-    QTimer *m_timer;
-    double m_renderTimeSimple;
-    double m_renderTimeHTML;
-
-    boost::posix_time::ptime m_appStartTime;
-
-protected slots:
-
-   void update();
-   void rateChanged(bool);
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#endif
->>>>>>> 9d18f777fde0dd673667c4b8b8ed4bff591f7162
